bd129ec0ea163e875f37101d078630dc561565fb
Matteo Barbero <66015486+maiobarbero@users.noreply.github.com>
2020-11-12 15:10:26 +0100

    Update connection.php
    Create todos table in DB

--- connection.php
+++ connection.php
@@ -10,6 +10,21 @@ try {
 } catch (PDOException $e) {
     echo 'Connection failed: ' . $e->getMessage();
 }
+/*Create DB Table*/
+$sql = "CREATE TABLE todos (
+id INT(11) AUTO_INCREMENT PRIMARY KEY, 
+title VARCHAR(200) NOT NULL,
+priority TEXT NOT NULL,
+type TEXT NOT NULL,
+date_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+completed TINYINT(1) NOT NULL DEFAULT 0
+)";
+
+if ($dbh->query($sql) === TRUE) {
+  echo "Table MyGuests created successfully";
+} else {
+  echo "Error creating table: " . $conn->error;
+}
 
 
 ?>
